976b7eeda95eba3b76f8f7e077ad3fc1d9e4a0d3
Milkah Mwangi <gathonimilkahh@gmail.com>
2025-11-24 15:18:49 +0300

    created a shopkeeping program using js

---
+++ shop-owner.js
@@ -0,0 +1,23 @@
+/*Create a mini JavaScript program that helps a shop owner calculate:
+
+Discount on an item
+Profit after selling the item
+Loss if the selling price is lower than the cost*/
+
+let cost  = 70
+
+let itemPrice=100
+let discount=itemPrice*0.05
+let sellingPrice=itemPrice-discount
+console.log("Selling Price is " + sellingPrice)
+
+if (sellingPrice > cost) {
+console.log ("Profit is " + (sellingPrice-cost))
+}
+
+else {
+    console.log ("Loss is " + (cost - sellingPrice))
+}
+
+
+
